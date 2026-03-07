e6b41307572691212560fcada09de29979734459
rohit <rohitdr098@gmail.com>
2026-03-07 17:45:55 +0000

    created route for login register and forget password

---
+++ Routes/auth.js
@@ -0,0 +1,125 @@
+const express= require('express')
+const bcrypt = require('bcryptjs')
+const router = express.Router();
+const User= require('../Modals/User')
+const jwt = require('jsonwebtoken')
+const {body,validationResult}=require('express-validator')
+
+
+
+
+
+//path to create a user
+router.post('/createUser',[
+     body('name').isLength({min:3}),
+     body('email').isEmail(),
+     body('username').isLength({min:8}),
+     body('phone_number').isLength({min:10,max:10}).isNumeric(),
+     body('password').isLength({min:8})
+
+],async(req,res)=>{
+    try{
+     let validationresult = validationResult(req)
+          if(!validationresult.isEmpty()){
+                 return res.status(404).json({error:validationresult.array()})
+          }
+     
+      const {email,password,name,username,phone_number}=req.body
+      let user = await User.findOne({email:email})
+     if(user){
+          return res.status(404).json({status:false,message:"This email already exits Use new one"})
+     }
+     user = await User.findOne({username:username})
+     if(user){
+          return res.status(404).json({status:false,message:"This Username already exits Use new one"})
+     }
+
+      const salt = await bcrypt.genSalt(10);
+      const securePassword = await bcrypt.hash(password,salt) 
+      user = await User.create({
+          email,
+          password:securePassword,
+          name,
+          phone_number,
+          username
+       })
+       const data={
+          id:user.id
+       }
+       const auth_token=jwt.sign(data,process.env.JWT_SECRET)
+       return res.status(200).json({status:true,auth_token})
+     }
+     catch(error){
+          return res.status(500).json({status:false,message:"Internal Servor Error"})
+     }
+})
+//path to login for the user
+router.post('/login',[ 
+     body('email').isEmail(),
+     body('password').isLength({min:8})],async(req,res)=>{
+          try{
+               let validationresult = validationResult(req)
+          if(!validationresult.isEmpty()){
+                 return res.status(404).json({error:validationresult.array()})
+          }
+     const {email,password}=req.body
+      let user= await User.findOne({email:email})
+      if(!user){
+           return res.status(404).json({status:false,message:"Please use Correct correndentials"})
+      }
+      let passCompare= await bcrypt.compare(password,user.password)
+      if(!passCompare){
+             return res.status(404).json({status:false,message:"Please use Correct correndentials"})
+      }
+      const data={
+          id:user.id
+      }
+      const auth_token=jwt.sign(data,process.env.JWT_SECRET)
+     return res.status(200).json({status:true,auth_token})
+          }
+          catch(error){
+               return res.status(500).json({status:false,message:"Internal Servor Error"})  
+          }
+})
+router.put('/forgetPassword',[
+      body('email').isEmail(),
+      body('username').isLength({min:8}),
+      body('password').isLength({min:8})
+     ],async(req,res)=>{
+          try{
+               let validationresult = validationResult(req)
+          if(!validationresult.isEmpty()){
+                 return res.status(404).json({error:validationresult.array()})
+          }
+          const {email,password,username}=req.body
+          let user = await User.findOne({email:email})
+          if(!user){
+              return res.status(404).json({status:false,message:"Use Correct Corredentials "})
+          }
+          if (username!==user.username){
+                 return res.status(404).json({status:false,message:"Use Correct Corredentials "})
+          }
+          const salt = await bcrypt.genSalt(10)
+          const newpassword = await bcrypt.hash(password,salt)
+           user = await User.updateOne(
+               {_id:user.id},
+               {$set:{password:newpassword}}
+           )
+           if(!user){
+                    return res.status(404).json({status:false,message:"Password not changed"})
+           }
+          return res.status(200).json({status:true,message:"password changed successfully"})
+
+
+
+
+          }
+           catch(error){
+               return res.status(500).json({status:false,message:"Internal Servor Error"})  
+          }
+})
+          
+
+
+
+module.exports= router
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express=require('express')
 const dotenv = require('dotenv')
+const  cors = require("cors");
 dotenv.config();
 const ConnectToMongoDb = require('./db')
 ConnectToMongoDb();
-
-
 const app = express();
+app.use(express.json())
+app.use(cors())
+app.use('/api/auth',require('./Routes/auth'))
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT,()=>{
     console.log("listening at port 5000")

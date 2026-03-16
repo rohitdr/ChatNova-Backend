4d3f2c35a010368ec4d3640462eaa2c71bd4fdc2
rohit <rohitdr098@gmail.com>
2026-03-16 06:49:48 +0000

    updatd notifiaction.js

--- Utils/sendNotification.cjs
+++ Utils/sendNotification.cjs
@@ -1,29 +1,32 @@
-const admin =require('../Config/firebase.cjs')
+const admin = require('../Config/firebase.cjs')
 
-const sendNotification =  async(message,sender,user)=>{
-const payload ={
-    notification:{
-        title:sender,
-        body:message
-    }
-}
-if(!user.deviceTokens || user.deviceTokens.length ===0){
+const sendNotification = async (message, sender, user) => {
+
+  if (!user.deviceTokens || user.deviceTokens.length === 0) {
     return
-}
-try{
-    const promise= await user.deviceTokens.map(token=>{
- return admin.messaging().send({
-     token,
-     notification:payload.notification
-})
-    })
-    await Promise.all(promise)
+  }
 
-}catch(error){
-    console.log(error)
-   
-}
-}
+  try {
 
+    const messagePayload = {
+      tokens: user.deviceTokens,
+      notification: {
+        title: sender,
+        body: message
+      },
+      webpush: {
+        notification: {
+          icon: "/favicon.ico"
+        }
+      }
+    }
+
+    await admin.messaging().sendEachForMulticast(messagePayload)
+
+  } catch (error) {
+    console.log("FCM error:", error)
+  }
+
+}
 
 module.exports = sendNotification

83039ec0442c3c47c875ee82d2bdf5383ac3296d
rohit <rohitdr098@gmail.com>
2026-03-16 13:30:38 +0000

    created a route to send conversation id

--- Routes/messages.cjs
+++ Routes/messages.cjs
@@ -131,6 +131,28 @@ router.post('/sendFile/:id',fetchUser,async(req,res)=>{
           
      }
 })
+//route to get conversation id 
+router.get('/conversationId/:id',fetchUser,async(req,res)=>{
+ try{    
+
+ 
+          const senderId = req.user.id
+          const receiverId=req.params.id
+          let chat = await Conversation.findOne({
+            participents:{$all:[senderId,receiverId]}
+          })
+          if(!chat){
+            chat = await Conversation.create({
+                participents:[senderId,receiverId]
+            })
+          }
+           return res.status(200).json({status:true,conversation:chat})
+ }
+  catch(error){
+          return res.status(500).json({status:false,message:error.message})
+          
+     }
+})
 
 
 
